f23f4d63bd176cb079aa22e5a5b2823f522c6835
Adegorite Adolabi David <adeadavid@gmail.com>
2021-01-27 23:08:22 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+    title: "My App",
+  });
+
+  win.setTitle("My App");
+  //   win.loadFile("index.html");
+  win.loadURL("http://localhost:3000/");
+  //   win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
